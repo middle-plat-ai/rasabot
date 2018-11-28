589782690f9977d9a83db0197000dc6a388e0495
algteam <41173624+algteam@users.noreply.github.com>
2018-11-28 14:14:31 +0800

    Update start-server.sh

--- rasabot-demo/start-server.sh
+++ rasabot-demo/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #启动nlu服务
-nohup python -m rasa_nlu.server -c sample_configs/config_hanlp_mitie_sklearn.yml --path models >> logs/nlu_logs.txt 2>&1 &
+nohup python -m rasa_nlu.server -c sample_configs/config_hanlp_mitie_sklearn.yml --path models >> nlu_logs.txt 2>&1 &
 
 #启动core服务
-nohup python -m rasa_core.server -d models/dialogue --debug >> logs/dialogue_logs.txt 2>&1 &
+nohup python -m rasa_core.server -d models/dialogue --debug >> dialogue_logs.txt 2>&1 &
